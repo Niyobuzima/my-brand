3c2fd94df76e0a39f1efd3e8321205c540987d2e
niyobuzima <theophileniyobuzima@gmail.com>
2023-02-15 03:42:29 -0600

    adding authentication to sign in button

---
+++ auth.js
@@ -0,0 +1,30 @@
+function signIn(){
+    // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '61084999943-utq5dufrajnjath1gm5vd3v6ce3u5eit.apps.googleusercontent.com',
+                'redirect_uri': 'http://127.0.0.1:5500/Admin%20panel/admin%20panel.html',
+                'response_type': 'token',
+                'scope':'https://www.googleapis.com/auth/userinfo.profile',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
